695de401922d2d1ca77e1be7c4619b3367d26256
tanviranjum1 <anz.tanvir@gmail.com>
2023-04-19 16:10:33 +0600

    deploy

---
+++ frontend/build/precache-manifest.157c935f3d335dde15dcceaf157f03b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d051cb1b0fe204da1916b618c7ba2d73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "587dd7f03990f6b71e58",
+    "url": "/static/css/main.c458e5b7.chunk.css"
+  },
+  {
+    "revision": "88fd899e1b44ea910291",
+    "url": "/static/js/2.11a1d88b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.11a1d88b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "587dd7f03990f6b71e58",
+    "url": "/static/js/main.4ec5caed.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Form, Button, FormControl } from "react-bootstrap";
+import { Form, Button } from "react-bootstrap";
 
 const SearchBox = ({ history }) => {
   const [keyword, setKeyword] = useState("");
